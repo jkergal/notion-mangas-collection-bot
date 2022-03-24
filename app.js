09f216f47c0511bf7cea6458c6be91012797b21d
Johann Kergal <hello@johannkergal.fr>
2022-03-24 11:09:12 +0100

    clean a bit of code

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const { Client } = require("@notionhq/client")
 
 dotenv.config()
 
-const DATABASE_ID = "6d6c9b8682ea4c9e9747efb7b6421d34"
-
 const notion = new Client ({
     auth: process.env.API_TOKEN
 })
@@ -34,7 +32,6 @@ async function updateNotificationsDb(manga) {
 }
 
 async function updateCollectionDb(volumeNumber, manga, mangaPageId) {
-    const narutoPageId = "3440e11c85a048fa968b8a4d63082cb4"
     const vol = volumeNumber
     const response = await notion.pages.update({
       page_id: mangaPageId,
@@ -60,19 +57,16 @@ async function searchMangaInDb(manga, volumeNumber) {
     mangaPage.push(mangaPageResult.results[i]);
   }
 
-  console.log(mangaPage[0].id);
+  // console.log(mangaPage[0].id);
 
   let mangaPageId = mangaPage[0].id
 
   updateCollectionDb(volumeNumber, manga, mangaPageId)
 }
 
-async function openSite() {
+async function launchScrapping() {
     // Get Manga Card page Url
     const mangaName = await prompt("Quel Manga t'intéresse? ")
-    // const mangaSelector = await `[title="Serie manga - ${mangaName}"]`
-    // console.log(mangaName)
-    // console.log(mangaSelector)
     const browser = await puppeteer.launch({headless: false, ignoreDefaultArgs: ['--disable-extensions']})
     const page = await browser.newPage()
     await page.goto(`https://www.manga-news.com/index.php/recherche/?q=${mangaName}`)
@@ -80,18 +74,9 @@ async function openSite() {
     await page.waitForSelector("#searchAccordion")
 
     const mangaUrl = await page.evaluate((manga) => {
-        // const manga = prompt("Retape le nom de ton manga pour confirmer : ")
+
         return document.querySelector(`[title="Serie manga - ${manga}"]`).href
 
-        // WIP : get the element that matches with whta we want
-
-        // let elements = document.querySelector("#searchAccordion > div:nth-child(1) > ul ").childNodes
-        // let table = []
-        // for (let i = 0; i < elements.length; i++) {
-        //     if (elements[i].nodeName = "#comment") {
-        //         table.push(elements[i].data)
-        //     }
-        //   }
     }, mangaName)
     
 
@@ -113,10 +98,9 @@ async function openSite() {
 
     console.log(`Coucou Jess, ici le bot manga de Jojo. \n Le manga ${mangaName} en est rendu au volume : ${lastVolume}`)
 
-    // updateCollectionDb(lastVolume, mangaName)
     searchMangaInDb(mangaName, lastVolume)
     
 }
 
 
-openSite()
+launchScrapping()
